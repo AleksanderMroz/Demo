a523143dd33f2c4c1cc19d9a1a61dd27396e8635
AleksanderMroz <aleksanderm@autograf.pl>
2018-09-22 21:28:30 +0200

    CustomerEntity

---
+++ src/main/java/my/AleksanderMroz/Demo/entity/CustomerEntity.java
@@ -0,0 +1,69 @@
+package my.AleksanderMroz.Demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMER")
+
+public class CustomerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false, length = 50)
+    private String customerName;
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Column(nullable = false, length = 200)
+    private String address;
+
+    protected CustomerEntity() {
+    }
+
+    public CustomerEntity(Long id, String customerName, String password, String address) {
+        this.id = id;
+        this.customerName = customerName;
+        this.password = password;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
