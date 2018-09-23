eb62cd74d0aba3d7772e609d81ab506e28b31c7d
AleksanderMroz <aleksanderm@autograf.pl>
2018-09-23 20:35:33 +0200

    Trying to start application on localhost 8095

--- src/main/java/my/AleksanderMroz/Demo/DemoApplication.java
+++ src/main/java/my/AleksanderMroz/Demo/DemoApplication.java
@@ -1,15 +1,31 @@
 package my.AleksanderMroz.Demo;
 
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableJpaRepositories
+@EntityScan
+@ComponentScan ("my.AleksanderMroz.Demo")
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		System.setProperty("spring.profiles.active", "mysql");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
 }
--- src/main/java/my/AleksanderMroz/Demo/controller/HomeController.java
+++ src/main/java/my/AleksanderMroz/Demo/controller/HomeController.java
@@ -1,11 +1,14 @@
 package my.AleksanderMroz.Demo.controller;
 
 
+
+
 import my.AleksanderMroz.Demo.constants.ModelConstants;
 import my.AleksanderMroz.Demo.constants.ViewNames;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -13,18 +16,13 @@ public class HomeController {
     protected static final String WELCOME = "WELCOME";
 
 
-    @GetMapping(value = "/")
+    @RequestMapping(value = "/")
     public String welcome(Model model) {
         model.addAttribute(ModelConstants.MESSAGE, WELCOME);
         model.addAttribute(ModelConstants.INFO, INFO_TEXT);
         return ViewNames.WELCOME;
     }
 
-    @GetMapping(value = "/login")
-    public String login(Model model) {
-
-        return "login";
-    }
 
 
 }
