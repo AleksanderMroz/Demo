fccedd60b4865c543747484718761010aa5e2319
AleksanderMroz <aleksanderm@autograf.pl>
2018-10-23 22:38:20 +0200

    Shipment list - basic list of id of ours shipments

--- src/main/java/my/AleksanderMroz/Demo/controller/ShipmentController.java
+++ src/main/java/my/AleksanderMroz/Demo/controller/ShipmentController.java
@@ -1,6 +1,10 @@
 package my.AleksanderMroz.Demo.controller;
 
 
+import my.AleksanderMroz.Demo.enumeration.ShipmentStatus;
+import my.AleksanderMroz.Demo.service.CustomerService;
+import my.AleksanderMroz.Demo.service.ShipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +13,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/shipments")
 public class ShipmentController {
+    @Autowired
+    ShipmentService shipmentService;
+    @Autowired
+    CustomerService customerService;
 
     @GetMapping()
     public String showBookList(Model model) {
+        model.addAttribute("shipmentList", customerService.findCustomerByName("Aleksander"));
         return "shipments";
     }
+
+    @GetMapping("/order")
+    public String showOrderPage(Model model) {
+        return "orderpage";
+    }
+
+
 }
